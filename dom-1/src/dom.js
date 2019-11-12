76e55f2bd9c1262b329a4acc522749e90856fa82
zhangdeshou <xjlady@126.com>
2019-11-12 12:08:44 +0800

    init

--- dom-1/src/dom.js
+++ dom-1/src/dom.js
@@ -50,6 +50,7 @@ window.dom = {
         //在下面的for循环中 remove节点之后 节点长度在变化就不能用
         //i < childNodes.length so用while循环
         while (x) {
+
             array.push(dom.remove(node.firstChild))
             x = node.firstChild
         }
@@ -61,6 +62,145 @@ window.dom = {
         // }
         //返回被删除的节点们组成的数组
         return array
+    },
+
+
+    //改
+
+    //设置title属性
+    attr(node, name, value) {//重载 根据参数个数不同写代码
+        if (arguments.length === 3) {
+            node.setAttribute(name, value)
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name)
+        }
+
+    },
+    text(node, string) {// 适配
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string
+            }
+            else {
+                node.innerContent = string
+            }
+        } else if (arguments.length === 1) {
+
+            if ('innerText' in node) {
+                return node.innerText
+            }
+            else {
+                return node.innerContent
+            }
+        }
+    },
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        }
+        else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            //dom.style(div,'color','red')
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                //dom.style(div,{'color':'red'})
+                const object = name
+                for (let key in object) {
+                    // key: border/color
+                    //node.style.border
+                    //node.style.color
+                    //key是个变量 如果用.key .key直接就变成字符串了
+                    node.style[key] = object[key]
+                }
+            }
+        }
+
+    },
+    class: {
+        add(node, className) {
+            node.classList.add(className)
+        },
+        remove(node, className) {
+            node.classList.remove(className)
+        },
+        has(node, className) {
+            return node.classList.contains(className)
+        }
+    },
+    //dom.on dom.off用于添加 删除监听事件
+    //test.addEventListener('click',fn) dom自带的api
+    on(node, eventName, fn) {
+        node.addEventListener(eventName, fn)
+    },
+    off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn)
+    },
+
+    // 查
+    find(selector, scope) {
+        //All读取的是全部 一个数组
+        //scope 限定范围 制定范围里面的selector 
+        // a||b 如果a真就执行，否真b 保底值b
+        return (scope || document).querySelectorAll(selector)
+    },
+    parent(node) {
+        return node.parentNode
+    },
+    children(node) {
+        return node.children
+    },
+    siblings(node) {
+        //伪数组转换为数组，， 才有数组的方法filter   
+        //(n => n !== node)这个节点不等于你传过来的节点   是就过滤掉
+        return Array.from(node.parentNode.children).filter(n => n !== node)
+    },
+    next(node) {
+        let x = node.nextSibling
+        //存在 而且不是文本的情况执行while循环
+        while (x && x.nodeType === 3) {
+            x = x.nextSibling
+        }
+        return x
+    },
+    previous(node) {
+        let x = node.previousSibling
+        //存在 而且不是文本的情况执行while循环
+        while (x && x.nodeType === 3) {
+            x = x.previousSibling
+        }
+        return x
+    },
+    // dom.each  dom.index
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    },
+    // index(node) {
+    //     const list = dom.children(node.parentNode)
+    //     for (let i = 0; i < list.length; i++) {
+    //         if (list[i] === node) { break }
+    //     }
+    //     return i
+    // }
+
+    index(node) {
+        const list = dom.children(node.parentNode)
+        let i  //let i的作用域问题
+        for (i = 0; i < list.length; i++) {
+            if (list[i] === node) { break }
+        }
+        return i
     }
 
-} 
+}
+
+
+
--- dom-1/src/main.js
+++ dom-1/src/main.js
@@ -12,3 +12,57 @@ dom.wrap(test, div3)
 
 const nodes = dom.empty(window.empty)
 console.log(nodes)
+
+
+dom.attr(test, 'title', 'hi,i am title')
+const title = dom.attr(test, 'test')
+console.log('title:${}')
+
+
+dom.text(test, 'hello 这是`dom.text`新的内容')
+dom.text(test)
+
+//写 读 对象-设置 字符串-获取值
+dom.style(test, { border: '1px solid blue', color: 'green' })  //设置
+const jieshou = dom.style(test, 'border')  //读 设置
+console.log(jieshou)
+dom.style(test, 'border', '1px solid red') //设置
+
+dom.class.add(test, 'red')
+dom.class.add(test, 'blue')
+dom.class.remove(test, 'blue')
+console.log(dom.class.has(test, 'red'))
+
+//test.addEventListener('click')
+const fn = () => {
+    console.log('点击了')
+}
+dom.on(test, 'click', fn)
+dom.off(test, 'click', fn)
+
+console.log(`--------找节点-------------`)
+const testDiv = dom.find('#test')[0]
+console.log(testDiv)
+const test2 = dom.find('#test2')[0]
+//找到的多个节点 打出的是一个节点
+console.log(dom.find('.red', test2))
+console.log(dom.find('.red', test2)[0])
+
+console.log(dom.parent(test))
+console.log(`--------找兄弟姐妹-------------`)
+
+// console.log(dom.siblings(dom.find('#e2')))
+console.log(dom.siblings(dom.find('#s2')[0]))
+console.log(`-----------找下一个节点----------`)
+// console.log(dom.next(dom.find('#s2')))   //随时记得find返回的是多个节点的数组
+console.log(dom.next(dom.find('#s2')[0])) //找到的是一个文本节点，我们其实要找的不是文本节点
+console.log(`-----------找上一个节点----------`)
+console.log(dom.previous(dom.find('#s2')[0]))
+
+console.log(`-----------遍历----------`)
+const t = dom.find('#travel')[0]
+console.log(t)
+dom.each(dom.children(t), (n) => dom.style(n, 'color', 'red'))
+
+console.log(`-----------排行老几----------`)
+console.log(dom.index(s2))
